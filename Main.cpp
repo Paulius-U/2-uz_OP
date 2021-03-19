3b4d650c7fdd92f28b2609c9ad4dd05117a8d7e4
Paulius-U <75576100+Paulius-U@users.noreply.github.com>
2021-03-19 12:56:57 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,7 @@ int main()
     cout << "duomenis nuskaityti nuo failo (1) ar irasote patys (0), ";
     cout << "sukurti faila ir ji naudoti (2): ";
     cin >> random;
-    if (random == 1) Skaitymas(A);
+    if (random == 1) Skaitymas(A, 0);
     else    if (random == 0) Ivedimas(A);
             else    if (random == 2) Sukurimas(A);
     if(random != 0 || random != 1 || random != 2) 
