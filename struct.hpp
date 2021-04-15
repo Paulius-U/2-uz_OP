a8259b2104a3fd34184abd0c10d0ba1b2e6e7213
Paulius-U <75576100+Paulius-U@users.noreply.github.com>
2021-04-15 21:02:02 +0300

    Update struct.hpp

--- struct.hpp
+++ struct.hpp
@@ -1,13 +1,8 @@
 #pragma once 
 
-#include <vector>
-
 struct duomenys
 {
     std::string Vardas;
     std::string Pavarde;
-    double Mediana = 0;
-    std::vector <int> Balai;
-    int Egzaminas;
     double Vidurkio_Balas;
 };
