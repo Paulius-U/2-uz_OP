bcc52892982f84cd0dd64cb3b41dd948cba07574
Paulius-U <75576100+Paulius-U@users.noreply.github.com>
2021-04-16 12:05:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,49 +9,68 @@
 int main()
 {
     char konteineris;
-    std::cout << "Pasirinkite konteineri: \n";
-    std::cout << "vector:   (0) \nlist  (1)\ndeque  (2) \n";
+    std::cout << "Pasirinkite konteineri: " << std::endl;
+    std::cout << "vector:   (0)" << std::endl;
+    std::cout << "list      (1)" << std::endl;
+    std::cout << "deque     (2)" << std::endl;
+    std::cout << "Pasirinkimas: ";
     std::cin >> konteineris;
     Pasirinkimas (konteineris, '0', '1', '2');
 
-    char failas;
-    std::cout << "Sugeneruoti failus kompiuterui Taip (1), Ne (0): \n";
-    std::cin >> failas;
-    Pasirinkimas (failas, '0', '1');
+    for(int i = 0; i < 3; i++)
+        std::cout << std::endl;
 
-    //Failo generavimas
-    if (failas == '1')
-    {
-        int failas = 1000;
-        std::cout << "Failu sukurimas uztruko: \n";
-        for(int i = 0; i < 4; i++)
-        {
-            auto Start = std::chrono::high_resolution_clock::now();
-            Sukurimas(failas);
-            std::cout << "kursiokai" << failas << ".txt ";
-            failas = failas * 10;
-            auto End = std::chrono::high_resolution_clock::now();
-            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count() / 1000.<< " s."<< std::endl;
-        }
-    }
+    // char failas;
+    // std::cout << "Sugeneruoti failus kompiuterui: (1/0) ";
+    // std::cin >> failas;
+    // Pasirinkimas (failas, '0', '1');
 
-    char Data;
-    std::cout << "Kaip viskas vyks: \n";
-    std::cout << "Naudosime egzistuojanti faila    (0) \n";
-    std::cout << "Generuosime faila   (1) \n";
-    std::cout << "Duomenis irasote patys   (2) \n";
-    std::cin >> Data;
-    Pasirinkimas(Data, '0', '1', '2');
+    // for(int i = 0; i < 3; i++)
+    //     std::cout << std::endl;
 
-    char Pazymys;
-    std::cout << "Pazymiu isvedimas vidurkio: \n";
-    std::cout << "Mediana   (0)\n";
-    std::cout << "Vidurkis  (1)\n";
-    std::cin >> Pazymys;
-    Pasirinkimas(Pazymys, '0', '1');
+    // if (failas == '1')
+    // {
+    //     int kiek = 1000;
+    //     std::cout << "Failu sukurimas uztruko: " << std::endl;
+    //     for(int i = 0; i < 4; i++)
+    //     {
+    //         auto Start = std::chrono::high_resolution_clock::now();
+    //         Sukurimas(kiek);
+    //         std::cout << "kursiokai" << kiek << ".txt - ";
+    //         kiek = kiek * 10;
+    //         auto End = std::chrono::high_resolution_clock::now();
+    //         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count() / 1000.<< " s."<< std::endl;
+    //     }
+    // }
+    // for(int i = 0; i < 3; i++)
+    //     std::cout << std::endl;
 
-    if (konteineris == '0') Vectorius(Data, Pazymys);
-    if (konteineris == '1') Listas(Data, Pazymys);
-    if (konteineris == '2') Dequelas(Data, Pazymys);
+    // char Data;
+    // std::cout << "Kaip viskas vyks: " << std::endl;
+    // std::cout << "Naudosime egzistuojanti faila    (0) " << std::endl;
+    // std::cout << "Kompiuteris generuos duomenis    (1) " << std::endl;
+    // std::cout << "Duomenis irasote patys           (2) " << std::endl;
+    // std::cout << "Pasirinkimas: ";
+    // std::cin >> Data;
+    // Pasirinkimas(Data, '0', '1', '2');
+
+
+    // for(int i = 0; i < 3; i++)
+    //     std::cout << std::endl;
+
+
+    // char Pazymys;
+    // std::cout << "Pazymiu isvedimas vidurkio: " << std::endl;
+    // std::cout << "Mediana   (0) ar Vidurkis  (1)" << std::endl;
+    // std::cout << "Pasirinkimas: ";
+    // std::cin >> Pazymys;
+    // Pasirinkimas(Pazymys, '0', '1');
+
+    // for(int i = 0; i < 3; i++)
+    //     std::cout << std::endl;
+
+    if (konteineris == '0') Vectorius('0', '1');
+    if (konteineris == '1') Listas('0', '1');
+    if (konteineris == '2') Dequelas('0', '1');
     return 0;
 }
